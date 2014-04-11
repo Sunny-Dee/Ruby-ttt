9083b4f9e5be92819a11fc49a97e55f9f7686003
Sunny-Dee <delianaescobari@gmail.com>
2014-04-11 15:55:42 -0500

    players can now take turns and choose a spot on the board

--- lib/ttt.rb
+++ lib/ttt.rb
@@ -1,4 +1,8 @@
 class Game
+	def initialize
+		@board = ["0","1","2","3","4","5","6","7","8"]
+	end
+
 	def greet
 		puts "Welcome!"
 	end
@@ -8,16 +12,29 @@ class Game
 	end
 
 	def make_turn 
-		puts "Please input x or o"
-		gets 
+		puts "Please input the cell you want"
+		gets.to_i 
 	end 
 
 
 	def play
 		greet
-		turn = make_turn
-		puts turn 
+		draw_board 
+		@board.insert(make_turn, 'x')
+		print draw_board
+		@board.insert(make_turn, 'o')
+		print draw_board
 		finish
+	end
+
+	def draw_board
+		puts " #{@board[0]}   |  #{@board[1]}     |  #{@board[2]}  "
+		puts "_____|________|_____"
+		puts " #{@board[3]}   |  #{@board[4]}     |  #{@board[5]}  "
+		puts "_____|________|_____"
+		puts " #{@board[6]}   |  #{@board[7]}     |  #{@board[8]}  "
+		puts "     |        |     "
+
 	end
 
 end
